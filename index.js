2cbf1b303b303e7621012c309eeb41f934b35669
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-02 14:16:49 -0800

    successfully added delete

--- index.js
+++ index.js
@@ -54,13 +54,16 @@ server.get('/api/users/:id', (req, res) => {
 
 //POST individual
 server.post('/api/users', (req, res) => {
-    const nameBio = req.body
-    const nameIf = req.body.name
-    const bioIf = req.body.bio
-console.log(nameBio)
-    db.insert(nameBio)
+    let nameBio = {}
+    if(req.body.name && req.body.bio){
+      nameBio = req.body
+    } else{
+       nameBio = null
+    }
+
+    db.insert(req.body)//without a req name the insert function will just fail and go to catch
     .then(dbentry => {
-        if(dbentry.bio == null || dbentry.name == null){
+        if(nameBio === null){
              //if there is no name it wont even reach here, it will go straight to the catch   
             res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
                
@@ -79,9 +82,26 @@ console.log(nameBio)
     })
 })
 
-//remove a hub by id
+//remove a user by id
 
-//update a hub by id and the changes
+server.delete(`/api/users/:id`, (req, res)=>{
+    const id = req.params.id
+
+    db.remove(id)
+    .then(numOfRecords => {
+        if(numOfRecords === 0){
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+        }else{
+            res.status(200).json({message: "user removed"})
+        }
+    })
+    .catch(err => {
+        console.log("error on DEL specific user", err)
+        res.status(500).json({error: "The user could not be removed"})
+    })
+})
+
+//update a user by id and the changes
 
 
 const port = 4000;
