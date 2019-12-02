f8a231197c2467b3f422159af5af661d5d3f3dc0
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-02 10:09:13 -0800

    first half of lecture using insomnia and setting up node and express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+//console.log("index.js ran");//can test index.js connectivity to current terminal
+
+//import express from 'express'; // ES Modules - node js does not support this
+
+const express = require("express"); //CommonJS Modules (import/Export in Node.js)
+
+const server = express() //creates our server
+
+server.get('/', (req, res)=> {
+    //access to 2 things, request object, response object
+res.send({api: "up and running..."})
+
+} ) //handle get requests (but which url?, what code do you want to run when they request to the url)
+
+const port = 4000;
+
+server.listen(port, () => {
+    console.log(`\n ** API running on port ${port} **\n`)
+})
---
+++ notes.md
@@ -0,0 +1,12 @@
+- install 'express' -> 'npm i express'
+-add index.js to root folder
+-add server code inside 'index.js'
+-run API using 'npm run server'
+-test making a GET req to 'localhost:4000' (can use postman or insomnia- insomnia is better because I can work offline with it)
+
+[client] send a req to a [server], the [server] then sends a response to the [client]
+
+    the res object will be returned in JSON (because all browsers operate on that)
+
+everything has been done on the root of the app
+
