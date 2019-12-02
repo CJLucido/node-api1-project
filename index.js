74e9b70478b54556beb2a08e6d46d953e2c23d8d
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-02 13:02:40 -0800

    post wasn't working because req was not coming in as JSON, server now using express.json

--- index.js
+++ index.js
@@ -9,27 +9,56 @@ const express = require("express"); //CommonJS Modules (import/Export in Node.js
 
 const server = express() //creates our server
 
+server.use(express.json())
+
 server.get('/', (req, res)=> {
     //access to 2 things, request object, response object
 res.send({api: "up and running..."})
 
 } ) //handle get requests (but which url?, what code do you want to run when they request to the url)
 
-//list of hubs GET /hubs <<<<2 implements endpoint
+//list from db GET /db
 
-server.get('/hubs', (req, res) => {
-    //get the list of hubs from the db
+server.get('/api/users', (req, res) => {
     db.find()
-    .then(hubs => {
-        res.status(200).json(hubs)//if you do not specify the status code, by default it will be 200, here we are making it clear that we wanted to
+    .then(dbentries => {
+        res.status(200).json(dbentries)
     })
     .catch(err=>{
-        console.log("error on GET /hubs", err)
-        res.status(500).json({errorMessage: "error getting list of hubs from database"}) //using json instead of send, so the next developer will have a visual indicator that you intend to send back json
+        console.log("error on GET /db", err)
+        res.status(500).json({errorMessage: "error getting list from database"}) 
     })
 })
 
-//add a hub
+
+//GET individual
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+
+
+    db.findById(id)
+    .then(entry => {
+        res.status(200).json(entry)
+    })
+    .catch(err=>{
+        console.log("error on GET /db/:id", err)
+        res.status(500).json({errorMessage: "error getting individual from database"}) 
+    })
+})
+
+//POST individual
+server.post('/api/users', (req, res) => {
+    const nameBio = req.body
+
+    db.insert(nameBio)
+    .then(dbentry => {
+        res.status(201).json(dbentry)
+    })
+    .catch(err=>{
+        console.log("error on POST /db", err)
+        res.status(500).json({errorMessage: "error inserting individual to database"}) 
+    })
+})
 
 //remove a hub by id
 
